7fcf514be1bc90456c594bfa78a7f0db6bad1898
Sarthak Kohli <Sarthak.kohli.0.7@gmail.com>
2018-03-01 00:18:41 +0530

    Create mnist_loader.py

---
+++ src/mnist_loader.py
@@ -0,0 +1,18 @@
+import pickle
+import gzip
+
+import numpy as np
+
+def load_data():
+    f = gzip.open('mnist.pkl.gz', 'rb')
+
+    training_data, validation_data, test_data = pickle.load(f, encoding = 'latin1')
+
+    f.close()
+
+    return (training_data, validation_data, test_data)
+
+def vectorized_result(j):
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+    return e
