dda7435e730419e2060e81796910c496015b1750
Sarthak Kohli <Sarthak.kohli.0.7@gmail.com>
2018-03-02 01:35:09 +0530

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,23 @@
+import network as net
+import numpy as np
+
+
+def feedforward(x,weights,weights1):
+
+    l = x.T
+    l1 = net.sigmoid(np.dot(l,weights))
+    l2 = net.sigmoid(np.dot(l1,weights1))
+    return l2;
+
+def check(te_inputs,te_outputs,weights,weights1):
+
+    correct = 0
+    
+    for i in range(len(te_inputs)):
+        
+        out = feedforward(te_inputs[i],weights,weights1)
+        f_out = np.argmax(out)
+        if(f_out == te_outputs[i]):
+            correct += 1
+
+    print ("Accuracy Of the Network is " , ((correct/10000)*100))
