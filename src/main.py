8290f3443aa1b44d00eb540d25da5e3bbd316fda
Sarthak Kohli <Sarthak.kohli.0.7@gmail.com>
2018-03-02 01:34:46 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import mnist_loader as ml
 import network as net
+import test as tt
 
 np.random.seed(1)
 
@@ -15,24 +16,12 @@ tr_outputs = [ml.vectorized_result(x) for x in tr_data[1]]
 for i in range(10000):
 
     weights , weights1 = net.train(tr_inputs[i],tr_outputs[i],weights,weights1)
-    if(i % 100) == 0 :
-        print (i/100, "%")
+    if(i % 500) == 0 :
+        print (i/500, "%")
 
 print ("Network Trained and ready to be operated")
 
 te_inputs = [np.reshape(x, (784,1)) for x in test_data[0]]
 te_outputs = test_data[1]
 
-correct = 0
-
-for i in range(len(te_inputs)):
-    
-    out = net.checker(te_inputs[i],weights,weights1)
-    f_out = np.argmax(out)
-    if(f_out == te_outputs[i]):
-        correct += 1
-
-print ("Accuracy Of the Network is " , ((correct/10000)*100))
-
-
-
+tt.check(te_inputs,te_outputs,weights,weights1)
