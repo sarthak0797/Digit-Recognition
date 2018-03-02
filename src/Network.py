c8ceb3f6688161baaa5c3af97d2e4feaa81bc430
Sarthak Kohli <Sarthak.kohli.0.7@gmail.com>
2018-03-02 23:36:35 +0530

    Update Network.py

--- src/Network.py
+++ src/Network.py
@@ -12,31 +12,28 @@ def train(inputs,output,weights,weights1):
     
     x = inputs.T
     y = output.T
+    """Giving the input to our network and calculating the outptut and
+        storing it in l2"""
+    
+    l1 = sigmoid(np.dot(x,weights))
+    l2 = sigmoid(np.dot(l1,weights1))
 
-    for i in range(1):
-        """Giving the input to our network and calculating the outptut and
-            storing it in l2"""
-        
-        l = x
-        l1 = sigmoid(np.dot(l,weights))
-        l2 = sigmoid(np.dot(l1,weights1))
+    """Calculating the error by subtracting our Networks output from expected output"""
+    error = y - l2 
 
-        """Calculating the error by subtracting our Networks output from expected output"""
-        error = y - l2 
+    """This gives how much did our output layer contributed in our missed output"""
+    l2_del = error * deriv_sigmoid(l2)
 
-        """This gives how much did our output layer contributed in our missed output"""
-        l2_del = error * deriv_sigmoid(l2)
+    
+    """Calculating the error of out hidden layer"""
+    error0 = l2_del.dot(weights1.T)
 
-        
-        """Calculating the error of our hidden layer"""
-        error0 = l2_del.dot(weights1.T)
+    """This gives how much did our hidden layer contributed in our missed output"""
+    l1_del = error0 * deriv_sigmoid(l1)
 
-        """This gives how much did our hidden layer contributed in our missed output"""
-        l1_del = error0 * deriv_sigmoid(l1)
-
-        """updating the values of our weights by how much we missed"""
-        weights1 += np.dot(l1.T,l2_del)
-        weights += np.dot(l.T,l1_del)
+    """updating the values of our weights by how much we missed"""
+    weights1 += np.dot(l1.T,l2_del)
+    weights += np.dot(x.T,l1_del)
 
     return weights,weights1
 
