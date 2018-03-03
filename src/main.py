728ffb3e923c38b47dfe33303b19520e3c47b71d
Sarthak Kohli <Sarthak.kohli.0.7@gmail.com>
2018-03-03 22:51:21 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 import numpy as np
 import mnist_loader as ml
-import Network as net
+import network as net
 import test as tt
+import bar as br
 
 np.random.seed(1)
 
 """randomly intitalising weights mapping input layer to hidden layer and
     hidden layer to output layer"""
 
-weights = 2*np.random.random((784,30)) - 1
-weights1 = 2*np.random.random((30,10)) - 1
+weights = 2*np.random.random((784,50)) - 1
+weights1 = 2*np.random.random((50,10)) - 1
 
 """Loading the MNIST data set into three lists two containg the training data
     and the third one containing the test data"""
@@ -25,11 +26,14 @@ tr_inputs = [np.reshape(x, (784, 1)) for x in tr_data[0]]
 tr_outputs = [ml.vectorized_result(x) for x in tr_data[1]]
 
 """Loop to train the data taking an input of 10,000 images"""
-for i in range(10000):
+for i in range(50000):
 
     weights , weights1 = net.train(tr_inputs[i],tr_outputs[i],weights,weights1)
-    if(i % 100) == 0 :
-        print (i/100, "%")
+    if(i % 500) == 0 :
+        br.progress(i, 50000)
+br.progress(50000, 50000, cond = True)
+
+print ("\n")
 
 print ("Network Trained and ready to be operated")
 
